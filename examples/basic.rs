d483e7a8b909a6371b2ebdee22073fba6c40b345
Dmitry Malkov <dmitry.malkov@live.com>
2022-05-17 23:07:42 -0600

    Add build script for paddle_inference_c lib

--- examples/basic.rs
+++ examples/basic.rs
@@ -6,6 +6,6 @@ fn main() {
     config.disable_glog_info();
     config.set_cpu_math_library_num_threads(1);
     config.set_model_dir("./lac_models/lac_model/model");
-    
+
     println!("basic example {:?}", config.get_raw_config_ptr());
 }
--- paddle-inference-api-sys/build.rs
+++ paddle-inference-api-sys/build.rs
@@ -2,10 +2,13 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
+use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
-    let manifest_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let paddle_install_dir = manifest_path.join("Paddle/build/");//PathBuf::from("./Paddle/build/");
+    let paddle_build_path = build_paddle();
+    let paddle_install_dir = paddle_build_path;
     let third_party_install_dir = paddle_install_dir.join("paddle_inference_c_install_dir/third_party/install");
     
     println!("cargo:rerun-if-changed=paddle_wrapper.h");
@@ -47,13 +50,91 @@ fn main() {
         .expect("Couldn't write bindings!");
 }
 
-fn build_paddle() {
-    // build manually for now
-    // cd Paddle
-    // mkdir build && cd build
-    // cmake -DWITH_TESTING=OFF -DWITH_PYTHON=OFF -DWITH_AVX=OFF -DWITH_MKL=OFF -DWITH_GPU=OFF -DON_INFER=ON -DCMAKE_BUILD_TYPE=Release ..
-    // make paddle_inference_c -j4
+fn run(cmd: &mut Command) {
+    println!("cargo:warning=running: {:?}", cmd);
 
-    todo!();
+    let status = match cmd.status() {
+        Ok(status) => status,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            fail(&format!(
+                "failed to execute command: {}\nis not installed?",
+                e
+            ));
+        }
+        Err(e) => fail(&format!("failed to execute command: {}", e)),
+    };
+    if !status.success() {
+        fail(&format!(
+            "command did not execute successfully, got: {}",
+            status
+        ));
+    }
+}
 
+fn build_paddle() -> PathBuf {
+    let manifest_path = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let install_dir = out_dir.join("build");
+
+    if !install_dir.join("CMakeCache.txt").exists() {
+        let _ = fs::create_dir(install_dir.clone());
+    
+        let mut cmake_configure_cmd = Command::new("cmake");
+    
+        cmake_configure_cmd.current_dir(install_dir.clone());
+        cmake_configure_cmd.env_remove("TARGET");
+        cmake_configure_cmd.args([
+            "-DWITH_TESTING=OFF",
+            "-DWITH_PYTHON=OFF",
+            "-DWITH_AVX=OFF",
+            "-DWITH_MKL=OFF",
+            "-DWITH_GPU=OFF",
+            "-DON_INFER=ON",
+            // "-DPY_VERSION=3.10",
+            "-DCMAKE_BUILD_TYPE=Release",
+            &format!("-DCMAKE_INSTALL_PREFIX={}", install_dir.clone().display()),
+            &format!("{}", manifest_path.join("Paddle").display())
+        ]);
+    
+        run(&mut cmake_configure_cmd);
+    }
+    else {
+        println!("cargo:warning=cmake configuration found. Skipping config step");
+    }
+
+    if !install_dir.join("bin").join("paddle").exists() {
+        let mut cmake_build_cmd = Command::new("cmake");
+        cmake_build_cmd.current_dir(install_dir.clone());
+        cmake_build_cmd.env_remove("TARGET");
+        cmake_build_cmd.args([
+            "--build",
+            ".",
+            "--target",
+            "paddle_inference_c",
+            "--config",
+            "Release",
+            "--parallel",
+            "4",
+        ]);
+
+        run(&mut cmake_build_cmd);
+
+        let mut make_install_cmd = Command::new("make");
+        make_install_cmd.current_dir(install_dir.clone());
+        make_install_cmd.env_remove("TARGET");
+        make_install_cmd.args([
+            "install",
+            "-j4"
+        ]);
+        run(&mut make_install_cmd);
+    }
+    else {
+        println!("cargo:warning=paddle binary found. Skipping build step");
+    }
+
+    install_dir
+}
+
+fn fail(s: &str) -> ! {
+    panic!("\n{}\n\nbuild script failed, must exit now", s)
 }
